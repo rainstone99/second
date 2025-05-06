4e845ae3400985b62fc23f6f54946d10e11f07a8
rainstone99 <kjdfjf@gmail.com>
2025-05-06 23:02:22 +0900

    add hexagon wall

---
+++ Game/Assets/Scripts/Wall.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2D;
+    public float moveSpeed = 3f;
+
+    void Start()
+    {
+        rigidbody2D.rotation = Random.Range(0f, 360f);
+        transform.localScale = Vector3.one * 10f;
+    }
+
+ 
+    void Update()
+    {
+        transform.localScale -= Vector3.one * moveSpeed * Time.deltaTime;
+
+        if (transform.localScale.x <= 0.05f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
